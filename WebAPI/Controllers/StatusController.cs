a824ac47ad59a38c8e349e4c1045f395a005073c
Stephen Chung <Stephen.Chung@intexact.com>
2018-03-10 19:02:47 +0800

    Lock Hosting.CurrentStatus for thread safety.

--- WebAPI/Controllers/StatusController.cs
+++ WebAPI/Controllers/StatusController.cs
@@ -13,25 +13,27 @@ namespace iChen.Web
 
 			var orgPrefix = orgId + ":";
 
-			var status = new Hosting.Status()
-			{
-				Started = Hosting.CurrentStatus.Started,
-				IsRunning = Hosting.CurrentStatus.IsRunning,
-				Version = Hosting.CurrentStatus.Version,
-				Port = Hosting.CurrentStatus.Port,
-				OpenProtocol = Hosting.CurrentStatus.OpenProtocol
-			};
+			lock (Hosting.CurrentStatus) {
+				var status = new Hosting.Status()
+				{
+					Started = Hosting.CurrentStatus.Started,
+					IsRunning = Hosting.CurrentStatus.IsRunning,
+					Version = Hosting.CurrentStatus.Version,
+					Port = Hosting.CurrentStatus.Port,
+					OpenProtocol = Hosting.CurrentStatus.OpenProtocol
+				};
 
-			foreach (var entry in Hosting.CurrentStatus.Controllers.Where(kv => kv.Value.StartsWith(orgPrefix, StringComparison.OrdinalIgnoreCase))) {
-				var text = entry.Value.Substring(orgPrefix.Length);
-				status.Controllers[entry.Key] = text;
-			}
-			foreach (var entry in Hosting.CurrentStatus.Clients.Where(kv => kv.Value.StartsWith(orgPrefix, StringComparison.OrdinalIgnoreCase))) {
-				var text = entry.Value.Substring(orgPrefix.Length);
-				status.Clients[entry.Key] = text;
-			}
+				foreach (var entry in Hosting.CurrentStatus.Controllers.Where(kv => kv.Value.StartsWith(orgPrefix, StringComparison.OrdinalIgnoreCase))) {
+					var text = entry.Value.Substring(orgPrefix.Length);
+					status.Controllers[entry.Key] = text;
+				}
+				foreach (var entry in Hosting.CurrentStatus.Clients.Where(kv => kv.Value.StartsWith(orgPrefix, StringComparison.OrdinalIgnoreCase))) {
+					var text = entry.Value.Substring(orgPrefix.Length);
+					status.Clients[entry.Key] = text;
+				}
 
-			return Ok(status);
+				return Ok(status);
+			}
 		}
 
 		[HttpGet("test")]
